b61ef56e2493b13139572bfad3c37db3ec19fa5e
David Barr <david20708@gmail.com>
2025-08-23 22:39:48 -0700

    changing run10.sh to use new command line args

--- python/run10.sh
+++ python/run10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while :; do
     n2=$(< 10x10-5220-list.txt shuf -n1 | cut -d" " -f1)
-    python solve.py pieces_set_1/pieces_10x10.txt 10 $n2 2>&1 | tee 10x10/$n2.log
+    python solve.py --puzzle 10x10_1 --partial 10,$n2 2>&1 | tee 10x10/$n2.log
 done
